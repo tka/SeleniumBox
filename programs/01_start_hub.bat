6c1e75a050ce5dfa059f9ca30db899292636a526
tka <tkatka@gmail.com>
2014-04-01 02:32:36 +0800

    init

---
+++ programs/01_start_hub.bat
@@ -0,0 +1,2 @@
+cd c:\Users\IEUser\Desktop\selenium
+start java -jar selenium-server-standalone-2.40.0.jar -role hub
---
+++ programs/02_start_node.bat
@@ -0,0 +1,3 @@
+cd c:\Users\IEUser\Desktop\selenium
+start java -jar selenium-server-standalone-2.40.0.jar -role node  -hub http://localhost:4444/grid/register
+
