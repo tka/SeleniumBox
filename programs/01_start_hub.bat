d49e4410a42ab39e6caa7a970ee3a3b85b633af4
tka <tka@server.local>
2014-04-03 02:03:04 +0800

    update selenium bat file

--- programs/01_start_hub.bat
+++ programs/01_start_hub.bat
@@ -1,2 +1,3 @@
 cd c:\Users\IEUser\Desktop\selenium
+Set Path=%Path%;c:\Users\IEUser\Desktop\selenium
 start java -jar selenium-server-standalone-2.40.0.jar -role hub
--- programs/02_start_node.bat
+++ programs/02_start_node.bat
@@ -1,3 +1,4 @@
 cd c:\Users\IEUser\Desktop\selenium
+Set Path=%Path%;c:\Users\IEUser\Desktop\selenium
 start java -jar selenium-server-standalone-2.40.0.jar -role node  -hub http://localhost:4444/grid/register
 
